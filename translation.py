b2bfea76e8b0daa246744974a95411a4806c5d97
deon378 <70142311+deon378@users.noreply.github.com>
2020-11-10 16:55:42 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,18 +1,14 @@
 class Translation(object):
     START_MSG = """`channel button helper`
-   <b>made by @legendoftelegram</b>""" 
+   <b>made by @COMRADE_ACHAYAN</b>""" 
     HELP = """
 👉 <b>ask dev</b>
 <b>Bot developed by</b> @Legendoftelegram"""
     CH_LIST = """<b>empty</b>"""  
     CAP_TION = """
- ✪༺ ──•◈•─ ─•◈•──༻✪
- ᴄʜᴀɴɴᴇʟ ➧ @MM_MalluMovies
- ᴄʜᴀɴɴᴇʟ ➧ @MM_TamilMovies
- ᴄʜᴀɴɴᴇʟ ➧ @MalluMoviesALL
- ᴄʜᴀɴɴᴇʟ ➧ @Mallu_Rockers
- ᴄʜᴀɴɴᴇʟ ➧ @MM_TVSeries
- ᴄʜᴀɴɴᴇʟ ➧ @MM_NEW
- ᴄʜᴀɴɴᴇʟ ➧ @MM_OLD
- ᴄʜᴀɴɴᴇʟ ➧ @MM_Linkz
-🎗️ʝσιи 🎗️ ѕнαяє🎗️ ѕυρρσят🎗️"""
+
+<b>➠Cʜᴀɴɴᴇʟ : @qMp_FiLmS</b>
+
+<b>➠Cʜᴀɴɴᴇʟ : @TamilBlasterz</b>
+
+<b> 🎯 ʝσιи ѕнαяє ѕυρρσят 🎯</b>
